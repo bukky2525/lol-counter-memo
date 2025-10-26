f9940a27bbee7773ef0a379f33dc4c9b0692208a
bukky2525 <bukky2525@example.com>
2025-10-26 21:45:37 +0900

    Add LOL counter pick memo (index.html only)

--- Study/TextCount_read.py
+++
@@ -1,6 +0,0 @@
-import re
-from collections import Counter
-
-with open('text_analysis_result.txt', 'r', encoding='utf-8') as file:
-    content = file.read()
-    print(content)
--- Study/test01.py
+++
@@ -1,9 +0,0 @@
-# 入力を受け取り、空白で区切ってリストに変換
-numbers = input().split()
-
-# 文字列を整数に変換
-numbers = list(map(int, numbers))
-
-# 最大値と最小値を表示
-print("最大値:", max(numbers))
-print("最小値:", min(numbers))
